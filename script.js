84a7d3c639121e9e1ed54a022d9bad02306cc67e
ProSuomiGamer <noafin23@gmail.com>
2026-04-27 20:45:48 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+const players = [
+  {
+    name: "ProSuomiGamerYT",
+    points: 12,
+    rank: 1,
+    tiers: ["LT3", "LT3"]
+  },
+  {
+    name: "Muikkunen1",
+    points: 1,
+    rank: 2,
+    tiers: ["LT5"]
+  },
+  {
+    name: "coldified",
+    points: 1,
+    rank: 3,
+    tiers: ["LT5"]
+  },
+  {
+    name: "Swight",
+    points: 290,
+    rank: 4,
+    tiers: ["HT3", "LT3", "HT1"]
+  }
+];
+
+const leaderboard = document.getElementById("leaderboard");
+
+players.forEach(player => {
+  const div = document.createElement("div");
+  div.classList.add("player");
+
+  div.innerHTML = `
+    <div class="rank">#${player.rank}</div>
+    <div class="name">
+      ${player.name}<br>
+      <span class="points">${player.points} points</span>
+    </div>
+    <div class="tiers">
+      ${player.tiers.map(t => `<span>${t}</span>`).join("")}
+    </div>
+  `;
+
+  leaderboard.appendChild(div);
+});
